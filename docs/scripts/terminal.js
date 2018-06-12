e16a2e7bf575cfe0d2a70e09478f40159c9bb404
Taucher Christoph <taucher.ch@gmail.com>
2018-06-12 10:13:04 +0200

    SPA Static Hosting

---
+++ docs/scripts/terminal.js
@@ -0,0 +1,19 @@
+﻿Blazor.registerFunction('scrollIntoView', (element) => {
+    document.getElementById(element.id).scrollIntoView();
+    return true;
+});
+
+Blazor.registerFunction('readFile', (inputElementId) => {
+    inputElementId = inputElementId.replace('"', '').replace('"', '');
+    var filesElement = document.getElementById(inputElementId);
+    var file = filesElement.files[0];
+    return new Promise((resolve, reject) => {       
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onload = function () {
+            var arrayBuffer = reader.result
+            var bytes = new Uint8Array(arrayBuffer);
+            resolve(Array.from(bytes));
+        }
+    });
+});
