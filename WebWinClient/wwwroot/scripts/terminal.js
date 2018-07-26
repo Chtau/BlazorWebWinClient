35128e341ff57e197104fce39b765fc7b87be247
Christoph Taucher <taucher.ch@gmail.com>
2018-07-26 15:29:59 +0200

    change FileReader call

--- WebWinClient/wwwroot/scripts/terminal.js
+++ WebWinClient/wwwroot/scripts/terminal.js
@@ -1,16 +1,24 @@
 ﻿window.terminal = {
-    readFile: function (inputElementId, dotnetHelper) {
+    readFile: async function (inputElementId, dotnetHelper) {
         inputElementId = inputElementId.replace('"', '').replace('"', '');
         var filesElement = document.getElementById(inputElementId);
         var file = filesElement.files[0];
         var reader = new FileReader();
+
         reader.readAsArrayBuffer(file);
-        reader.onload = function () {
-            var arrayBuffer = reader.result
-            var bytes = new Uint8Array(arrayBuffer);
-            dotnetHelper.invokeMethodAsync('ReadFileReturn', Array.from(bytes));
-        }
-        return true;
+        return await new Promise((resolve, reject) => {
+            try {
+                reader.onload = function () {
+                    var arrayBuffer = reader.result
+                    var bytes = new Uint8Array(arrayBuffer);
+                    dotnetHelper.invokeMethodAsync('ReadFileReturn', Array.from(bytes));
+                    resolve(true);
+                }
+            } catch (e) {
+                reject(false);
+            }
+            
+        });
     },
     scrollIntoView: function (element) {
         document.getElementById(element.id).scrollIntoView();
